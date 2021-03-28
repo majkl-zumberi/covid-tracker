0f6ee23b270cdf6e5188a2604ee2d34bce2b38aa
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:14:36 +0200

    feat(Component): :construction: (in progress | Mockup) add CovMap component
    will contain map tracking covid

---
+++ src/components/CovMap.js
@@ -0,0 +1,11 @@
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+function CovMap() {
+  return (
+    <Grid item xs={12} sm={8}>
+      <div>Mappa</div>
+    </Grid>
+  );
+}
+
+export default CovMap;
