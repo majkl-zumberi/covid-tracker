8578f72dcccc47eb60fa880132bce9bc57c369ac
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:11:09 +0200

    feat(Component): :sparkles: add Container component
    will host some children comps

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
+import React from "react";
+function Container(props) {
+  return (
+    <Grid container style={{ padding: "1em 1em 0 1em" }}>
+      {props.children}
+    </Grid>
+  );
+}
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Container;
