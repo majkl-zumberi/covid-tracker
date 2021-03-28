f2ee19ed20ca181bb1c34dfca6cada7c160bf5cc
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:15:28 +0200

    fix(index): :sparkles: add theme to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Paper from "@material-ui/core/Paper";
+import { ThemeProvider } from "@material-ui/core/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme/theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    <Paper style={{ height: "100vh", width: "100vw" }}>
+      <App />
+    </Paper>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
