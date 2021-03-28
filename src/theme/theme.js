eb226cb8fb129b567ecad2a9a1dc0f8a131b27cd
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:09:49 +0200

    feat(theme): :sparkles: add material-ui theme

---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#512da8",
+    },
+    secondary: {
+      main: "#4527a0",
+    },
+  },
+  typography: {
+    fontSize: 14,
+  },
+});
+export default theme;
