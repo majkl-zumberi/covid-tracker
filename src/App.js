bec5b92f71b03a336254259bc14820d76e19498e
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:16:32 +0200

    fix(App): :construction: (Mockup) fix app component
    first mockup TODO:Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Container from "./components/Container";
+import CovDeaths from "./components/Cov-Deaths";
+import CovMap from "./components/CovMap";
+import ListContainer from "./components/ListContainer";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Container>
+        <ListContainer>
+          <CovDeaths />
+        </ListContainer>
+        <CovMap />
+        <ListContainer>
+          <CovDeaths />
+        </ListContainer>
+      </Container>
     </div>
   );
 }
