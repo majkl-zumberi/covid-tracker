8078a8598205d75433c22ef224b292311d1d6408
majkl zumberi <majklzumberi00@gmail.com>
2021-03-28 19:10:13 +0200

    feat(component): :sparkles: add navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,80 @@
+import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import InboxIcon from "@material-ui/icons/Inbox";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState } from "react";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  list: {
+    width: 250,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+function Nav() {
+  const [drawer, setDrawer] = useState({ open: false });
+  const toggleDrawer = (status) => {
+    setDrawer({ open: status });
+  };
+  const classes = useStyles();
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar color="primary">
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+          onClick={() => toggleDrawer(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Covid tracker
+        </Typography>
+      </Toolbar>
+      <Drawer
+        anchor="left"
+        open={drawer.open}
+        onClose={() => toggleDrawer(false)}
+      >
+        <div className={classes.list}>
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItem button>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="casi e decessi" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="vaccinazioni" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </AppBar>
+  );
+}
+
+export default Nav;
