1cb1afa34f28f291396049df01293baf69d1adf9
majkl zumberi <majklzumberi00@gmail.com>
2021-04-03 16:05:43 +0200

    feat(redux-action): :sparkles: handle loading and error in fetcCovTotals action & add fetchGlobalCov

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,14 +1,23 @@
 import axios from "axios";
-export const fetchCovTotals = () => {
-  // eslint-disable-next-line no-unused-vars
-  return async (dispatch, getState) => {
+export const fetchCovTotals = () => async (dispatch) => {
+  dispatch({ type: "FETCH_COVTOTALS_REQUEST" });
+  try {
     const { data } = await axios.get(
       "https://disease.sh/v3/covid-19/countries?sort=cases"
     );
-    console.log({ data });
-    dispatch({
-      type: "FETCH_COVTOTALS",
-      payload: data,
-    });
-  };
+    dispatch({ type: "FETCH_COVTOTALS_SUCCESS", payload: data });
+  } catch (error) {
+    dispatch({ type: "FETCH_COVTOTALS_FAILURE", error });
+  }
+};
+export const fetchGlobalCov = () => async (dispatch) => {
+  dispatch({ type: "FETCH_COVGLOBAL_REQUEST" });
+  try {
+    const { data } = await axios.get(
+      "https://disease.sh/v3/covid-19/all?allowNull=false"
+    );
+    dispatch({ type: "FETCH_COVGLOBAL_SUCCESS", payload: data });
+  } catch (error) {
+    dispatch({ type: "FETCH_COVGLOBAL_FAILURE", error });
+  }
 };
