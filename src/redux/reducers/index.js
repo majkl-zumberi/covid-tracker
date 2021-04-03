14f84a8a225287b7544adf06a1c5c0b28c99ea35
majkl zumberi <majklzumberi00@gmail.com>
2021-04-03 16:06:27 +0200

    fix(rootReducer): :sparkles: add globalCovReducer into rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import covTotalsReducer from "./covTotals";
+import globalCovReducer from "./globalCov";
 const rootReducer = combineReducers({
   covTotals: covTotalsReducer,
+  covGlobal: globalCovReducer,
 });
 export default rootReducer;
