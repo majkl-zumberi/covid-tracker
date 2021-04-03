ceac8ddb0e6c90365ac791efcc5e9afdcf306bae
majkl zumberi <majklzumberi00@gmail.com>
2021-04-03 16:08:21 +0200

    fix(Component): :zap: use of store into App Component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Container from "./components/Container";
 import CovList from "./components/CovList";
 import CovMap from "./components/CovMap";
 import ListContainer from "./components/ListContainer";
+import Loader from "./components/Loader";
 import Nav from "./components/Nav";
-import { fetchCovTotals } from "./redux/actions";
+import { fetchCovTotals, fetchGlobalCov } from "./redux/actions";
 function App() {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
@@ -14,20 +15,47 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchCovTotals());
+    dispatch(fetchGlobalCov());
   }, []);
-
+  const renderData = () => {
+    if (state.covGlobal.loading || state.covTotals.loading) {
+      return <Loader open={true} />;
+    } else {
+      return (
+        <Container>
+          <ListContainer>
+            <CovList
+              title="Totale decessi"
+              titleColor="error.main"
+              total={
+                state.covGlobal.detail.deaths &&
+                state.covGlobal.detail.deaths.toLocaleString()
+              }
+              items={state.covTotals.countries}
+              focus="deaths"
+            />
+          </ListContainer>
+          <CovMap />
+          <ListContainer>
+            <CovList
+              title="Totale Infetti"
+              titleColor="secondary.main"
+              total={
+                state.covGlobal.detail.deaths &&
+                state.covGlobal.detail.cases.toLocaleString()
+              }
+              items={state.covTotals.countries}
+              focus="cases"
+            />
+          </ListContainer>
+        </Container>
+      );
+    }
+  };
   return (
     <div className="App">
       <Nav />
-      <Container>
-        <ListContainer>
-          <CovList title="Totale decessi" total="2.777.188" />
-        </ListContainer>
-        <CovMap />
-        <ListContainer>
-          <CovList />
-        </ListContainer>
-      </Container>
+      {renderData()}
     </div>
   );
 }
