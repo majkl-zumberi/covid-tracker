606b44e88b010277a4a3b8801ff3a2a52840212e
majkl zumberi <majklzumberi00@gmail.com>
2021-04-03 16:04:09 +0200

    fix(redux-reducer): :sparkles: handle load and error in CovTotalsReducer

--- src/redux/reducers/covTotals.js
+++ src/redux/reducers/covTotals.js
@@ -1,8 +1,30 @@
-const initialState = [];
+const initialState = {
+  countries: [],
+  loading: false,
+  error: null,
+};
 const covTotalsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_COVTOTALS":
-      return action.payload;
+    case "FETCH_COVTOTALS_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case "FETCH_COVTOTALS_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        countries: action.payload,
+        error: null,
+      };
+    case "FETCH_COVTOTALS_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+        countries: [],
+      };
     default:
       return state;
   }
