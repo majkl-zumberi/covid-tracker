11f9a1a4724e4f7844ea5e3ada299d51b7ad2a95
majkl zumberi <majklzumberi00@gmail.com>
2021-04-03 11:59:13 +0200

    fix(Component): :sparkles: fetch api on component (app) mount
    using react hook, useEffect will be dispatched fetchCovTotals (redux action)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Container from "./components/Container";
-import CovDeaths from "./components/Cov-Deaths";
+import CovList from "./components/CovList";
 import CovMap from "./components/CovMap";
 import ListContainer from "./components/ListContainer";
 import Nav from "./components/Nav";
+import { fetchCovTotals } from "./redux/actions";
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  console.log({ state });
+
+  useEffect(() => {
+    dispatch(fetchCovTotals());
+  }, []);
+
   return (
     <div className="App">
       <Nav />
       <Container>
         <ListContainer>
-          <CovDeaths />
+          <CovList title="Totale decessi" total="2.777.188" />
         </ListContainer>
         <CovMap />
         <ListContainer>
-          <CovDeaths />
+          <CovList />
         </ListContainer>
       </Container>
     </div>
