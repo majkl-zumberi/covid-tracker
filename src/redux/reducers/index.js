7ad1379c07bb77691adf35ce1750f805339fbc53
majkl zumberi <majklzumberi00@gmail.com>
2021-04-04 17:09:52 +0200

    feat(redux-selectors): :sparkles: add redux  selectors
    provide also sorted countries by deaths and cases

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { createSelector } from "reselect";
 import covTotalsReducer from "./covTotals";
 import globalCovReducer from "./globalCov";
 const rootReducer = combineReducers({
@@ -6,3 +7,19 @@ const rootReducer = combineReducers({
   covGlobal: globalCovReducer,
 });
 export default rootReducer;
+
+//selectors
+export const selectCovGlobal = (state) => state.covGlobal;
+export const selectCovTotals = (state) => state.covTotals;
+export const selectCountries = (state) => state.covTotals.countries;
+
+//descending
+export const selectCountriesSortedByDeaths = createSelector(
+  selectCountries,
+  (countries) => countries.sort((a, b) => b.deaths - a.deaths)
+);
+//descending
+export const selectCountriesSortedByCases = createSelector(
+  selectCountries,
+  (countries) => countries.sort((a, b) => b.cases - a.cases)
+);
