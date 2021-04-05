0b4978f26584127f9f71e81e3288ff62fd7e239d
majkl zumberi <majklzumberi00@gmail.com>
2021-04-05 20:24:48 +0200

    fix(rootReducer): :sparkles: add coverage & global coverage to root reducer and add new selectors
    added selectTotalGlobalCoverage and selectTotalCoverageGroupedByCountries selectors

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import { createSelector } from "reselect";
+import coverageReducer from "./coverage";
 import covTotalsReducer from "./covTotals";
 import globalCovReducer from "./globalCov";
+import globalCoverageReducer from "./globalCoverage";
 const rootReducer = combineReducers({
   covTotals: covTotalsReducer,
   covGlobal: globalCovReducer,
+  coverage: coverageReducer,
+  globalCoverage: globalCoverageReducer,
 });
 export default rootReducer;
 
@@ -12,6 +16,12 @@ export default rootReducer;
 export const selectCovGlobal = (state) => state.covGlobal;
 export const selectCovTotals = (state) => state.covTotals;
 export const selectCountries = (state) => state.covTotals.countries;
+export const selectCoverage = (state) => state.coverage;
+export const selectCoverageCountries = (state) =>
+  state.coverage.coverageCountries;
+export const selectGlobalCoverage = (state) => state.globalCoverage;
+export const selectGlobalCoverageTimeline = (state) =>
+  state.globalCoverage.timeline;
 
 //descending
 export const selectCountriesSortedByDeaths = createSelector(
@@ -22,4 +32,39 @@ export const selectCountriesSortedByDeaths = createSelector(
 export const selectCountriesSortedByCases = createSelector(
   selectCountries,
   (countries) => countries.sort((a, b) => b.cases - a.cases)
+);
+
+//groups all timeline administrated vaccine doses, descending
+export const selectTotalCoverageGroupedByCountries = createSelector(
+  selectCoverageCountries,
+  selectCountries,
+  (coverages, countries) => {
+    // data exist
+    if (Array.isArray(coverages)) {
+      return coverages
+        .map((coverage) => {
+          const timelineArr = Object.entries(coverage.timeline);
+          const country = countries.find(
+            (country) => country.country == coverage.country
+          );
+          const countryInfo = country?.countryInfo ?? null;
+          return {
+            country: coverage.country,
+            totalCountryCoverage: timelineArr.reduce(
+              (tot, acc) => (tot += acc[1]),
+              0
+            ),
+            countryInfo: countryInfo,
+          };
+        })
+        .sort((a, b) => b.totalCountryCoverage - a.totalCountryCoverage);
+    }
+    return coverages;
+  }
+);
+
+// sum all global timeline administrated vaccine doses
+export const selectTotalGlobalCoverage = createSelector(
+  selectGlobalCoverageTimeline,
+  (timeline) => Object.entries(timeline).reduce((tot, acc) => tot + acc[1], 0)
 );
