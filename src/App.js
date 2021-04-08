9cd937f52bb5dc17c89ad4511aa25b1ea332bac7
majkl zumberi <majklzumberi00@gmail.com>
2021-04-09 00:11:04 +0200

    fix(Component): :sparkles: fetch fetchMobility from App Component
    in useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import Coverage from "./components/Coverage";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav";
+import Mobility from "./components/Mobility";
 import {
   fetchCovTotals,
   fetchGlobalCov,
   fetchCoverage,
   fetchGlobalCoverage,
+  fetchMobility,
 } from "./redux/actions";
 function App() {
   const dispatch = useDispatch();
@@ -18,17 +25,22 @@ function App() {
     dispatch(fetchGlobalCov());
     dispatch(fetchGlobalCoverage());
     dispatch(fetchCoverage());
+    dispatch(fetchMobility());
   }, []);
   return (
     <Router>
       <Nav />
       <Switch>
-        <Route path="/coverage">
+        <Route exact path="/coverage">
           <Coverage />
+        </Route>
+        <Route exact path="/mobility">
+          <Mobility />
         </Route>
         <Route exact path="/">
           <Home />
         </Route>
+        <Redirect from="*" to="/" />
       </Switch>
     </Router>
   );
